db8c17f609901cc12ce70e1a068e6f1edeeff9c9
eduardo <lalopdiaz35@gmail.com>
2025-12-25 19:34:54 -0800

    fix eliminada ruta de login

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import CategoriesPage from './pages/CategoriesPage';
 import ParticipantsPage from './pages/ParticipantsPage';
-import VotingPage from './pages/VotingPage';
-import LoginPage from './pages/LoginPage'; 
+import VotingPage from './pages/VotingPage';  
 import ResultsDashboard from './pages/ResultsDashboard'; 
 
 const App: React.FC = () => {
@@ -21,7 +20,7 @@ const App: React.FC = () => {
           <Route path="/categorias" element={<CategoriesPage />} />
           <Route path="/participantes" element={<ParticipantsPage />} />
           <Route path="/votacion" element={<VotingPage />} />
-          <Route path="/login" element={<LoginPage />} /> 
+          
           <Route path="/resultados" element={<ResultsDashboard />} />
         </Routes>
       </main>
